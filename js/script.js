8043a82ec825ac696f577f2683ee437a6e25e14d
Ali-GreenHeart-Khazar <alinemat.isiyev@khazar.org>
2022-02-25 07:36:29 +0400

    main commits

--- js/script.js
+++ js/script.js
@@ -1,19 +1,76 @@
-import { words}  from './words.js'  /* words arrayini daxil et */
-import {randomWordsTaker, writeElementToWordBox} from './utils.js'
+import { words } from "./words_DB.js"; /* words arrayini daxil et */
+import { randomWordsTaker, writeElementToWordBox, compareWords } from "./utils.js";
 
-const  wordBox=document.querySelector("#wordBox")
-const  enteredWord=document.querySelector("#enteredWord")
-const  time=document.querySelector("#time")
-const  resetBtn=document.querySelector("#resetBtn")
-
-
+const wordBox = document.querySelector("#wordBox");
+const enteredWord = document.querySelector("#enteredWord");
+const time = document.querySelector("#time");
+const resetBtn = document.querySelector("#resetBtn");
+const xalTablosu = document.querySelector("#xal");
 /*
 +1. sozlerden 15 eded random formada secin. 
 +2. secilen sozleri wordBox-un icerisine yazin
 */
 
+const newRandomWords = randomWordsTaker(15, words);
 
+writeElementToWordBox(newRandomWords, wordBox);
 
-const  newRandomWords = randomWordsTaker(15, words)
+let currentWordIndex = 0;
+let xal = 0;
+wordBox.childNodes[currentWordIndex + 1].className = "activeSpan";
 
-writeElementToWordBox(newRandomWords, wordBox)
+const finishTheGame = () => {
+  enteredWord.disabled = true;
+  clearInterval(intervalTimer);
+};
+
+let seconds = 60;
+
+const timer = () => {
+  if (seconds === 0) {
+    finishTheGame();
+  } else {
+    seconds = seconds - 1;
+    time.innerHTML = seconds + "s";
+  }
+};
+
+const intervalTimer = setInterval(timer, 1000);
+
+const classNameRemover = (index, isItOld) => {
+  for (let i = 0; i < index; i++) {
+    wordBox.childNodes[i].className = isItOld ? "oldWords" : "";
+  }
+};
+
+enteredWord.addEventListener("keydown", ({ keyCode }) => {
+  let arrayLength = newRandomWords.length;
+  if (currentWordIndex === arrayLength) {
+    alert("oyun bitdi");
+    //   neticelerGoster -> showResult
+  }
+  if (keyCode === 32 && currentWordIndex < arrayLength) {
+    wordBox.childNodes[currentWordIndex + 2].className = "activeSpan";
+    classNameRemover(currentWordIndex + 2, true);
+    if (compareWords(enteredWord.value, newRandomWords[currentWordIndex])) xal += 10;
+    else xal -= 10;
+
+    xalTablosu.innerHTML = xal;
+    enteredWord.value = "";
+    currentWordIndex++;
+  }
+});
+
+resetBtn.addEventListener("click", () => {
+  window.location.reload();
+});
+
+/*
++1. give style to current word!
++a. wordBox-un childNodes-larini gotur
++b. add activeSpan class to className of the object. 
++c. add oldWords class to className of the span
+
++2. time is not working!
++3. reset is not working
+*/
--- js/utils.js
+++ js/utils.js
@@ -1,52 +1,58 @@
 // utilizer -> yardimci
 
-
 /**
- * 
+ *
  * @param {*} maxIndex the maximum number which new random can be
  * @returns new random number from 0 to maxIndex parameter
  */
-export const randomizer = (maxIndex) => Math.floor(Math.random() * maxIndex)
+export const randomizer = (maxIndex) => Math.floor(Math.random() * maxIndex);
 // implementation -> icraati
 
 /**
- * 
+ *
  * @param {*} numberCount - the count of numbers
  * @param {*} words - the array of the words
- * @returns numberCount words from the words array. 
+ * @returns numberCount words from the words array.
  */
-export const randomWordsTaker=(numberCount, words)=>{
-    const maxRandomLimitIndex=words.length;
-    const randomWords = []
+export const randomWordsTaker = (numberCount, words) => {
+  const maxRandomLimitIndex = words.length;
+  const randomWords = [];
 
-      for( let i=0; i<numberCount; i++){
-        const randomIndex = randomizer(maxRandomLimitIndex); // 12
-        const randomWord=words[randomIndex];
-        randomWords.push(randomWord)
-      }
-      return randomWords;
-}
+  for (let i = 0; i < numberCount; i++) {
+    const randomIndex = randomizer(maxRandomLimitIndex); // 12
+    const randomWord = words[randomIndex];
+    randomWords.push(randomWord);
+  }
+  return randomWords;
+};
 
 /**
- * 
+ *
  * @param {*} word - the word
- * @returns new span element 
+ * @returns new span element
  */
-export const wordAdderToWordBox=(word)=>{
-  const newElement=document.createElement("span")
-  const spaninSozu=document.createTextNode(word);
-  newElement.appendChild(spaninSozu) 
+export const wordAdderToWordBox = (word) => {
+  const newElement = document.createElement("span");
+  const spaninSozu = document.createTextNode(word);
+  newElement.appendChild(spaninSozu);
   return newElement;
-}
+};
 
 /**
- * 
+ *
  * @param {*} newRandomWords - words array
  * @param {*} wordBox - wordBox itself
  */
-export const writeElementToWordBox=(newRandomWords, wordBox)=>{
-  for(let i=0;i<newRandomWords.length;i++){
-    const newElement = wordAdderToWordBox(newRandomWords[i])
-    wordBox.appendChild(newElement)
+export const writeElementToWordBox = (newRandomWords, wordBox) => {
+  for (let i = 0; i < newRandomWords.length; i++) {
+    const newElement = wordAdderToWordBox(newRandomWords[i]);
+    wordBox.appendChild(newElement);
   }
-}
+};
+/**
+ *
+ * @param {*} word1 - first string
+ * @param {*} word2 - second string
+ * @returns if they're same returns true, else returns false
+ */
+export const compareWords = (word1, word2) => word1.trim() === word2.trim();
